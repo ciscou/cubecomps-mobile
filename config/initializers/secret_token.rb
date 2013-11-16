c9b312e4dcbd2a24864da40563ffc2e3879b4354
ciscou <francismpp@gmail.com>
2013-11-16 19:39:19 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CubecompsMobile::Application.config.secret_key_base = '44640557265f97cae8f20ad170897d095320cd02245a21dd68ae21b0ac019a4aa178e4f2c73e02bbaeae7e08e88a5e7424cf7fefb395535ff130115d247c40cf'
