2f91c8ab6dbd7ff10b79e3548fc0ae30199b025f
ciscou <francismpp@gmail.com>
2013-11-17 16:48:18 +0100

    added past and upcoming competitions

--- app/models/competitions.rb
+++ app/models/competitions.rb
@@ -3,6 +3,14 @@ class Competitions
     @in_progress ||= fetch_in_progress
   end
 
+  def past
+    @past ||= fetch_past
+  end
+
+  def upcoming
+    @upcoming ||= fetch_upcoming
+  end
+
   def cache_key
     "competitions"
   end
@@ -10,8 +18,19 @@ class Competitions
   private
 
   def fetch_in_progress
-    doc = Nokogiri::HTML open "http://cubecomps.com/"
-    in_progress_competitions_tr = doc.css("div.list").first.css("table tr")
+    fetch_competitions_at(0)
+  end
+
+  def fetch_past
+    fetch_competitions_at(1)
+  end
+
+  def fetch_upcoming
+    fetch_competitions_at(2)
+  end
+
+  def fetch_competitions_at(index)
+    in_progress_competitions_tr = doc.css("div.list")[index].css("table tr")
     in_progress_competitions = in_progress_competitions_tr.css("td div").map do |competition_div|
       competition_url = competition_div.css("b.p a").attr("href").value
       competition_params = CGI.parse competition_url.split("?").last
@@ -20,5 +39,9 @@ class Competitions
         name: competition_div.css("b.p a").text.strip
       )
     end
+  end
+
+  def doc
+    @doc ||= Nokogiri::HTML open "http://cubecomps.com/"
   end
 end
