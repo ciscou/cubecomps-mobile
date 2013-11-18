e1951eacf536976db5b159b203a2712afe6e6e0e
ciscou <francismpp@gmail.com>
2013-11-18 20:26:17 +0100

    only 10 past competitions on competitions list

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -2,4 +2,8 @@ class CompetitionsController < ApplicationController
   def index
     @competitions = Competitions.new
   end
+
+  def past
+    @competitions = Competitions.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ CubecompsMobile::Application.routes.draw do
         resources :results, only: :index
       end
     end
+    get :past, on: :collection
   end
 
   root to: "competitions#index"
