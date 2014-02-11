70065bd7417d66dcbfef0484519f9e9838c82151
ciscou <francismpp@gmail.com>
2014-02-11 13:50:13 +0100

    ooops, actually fetch competitor id

--- app/models/competitor.rb
+++ app/models/competitor.rb
@@ -8,8 +8,12 @@ class Competitor
   end
 
   def self.build_from_competitor_div(competitor_div)
+    competitor_onclick = competitor_div.attr("onclick")
+    competitor_url     = competitor_onclick.slice(17..-2)
+    competitor_params  = CGI.parse competitor_url.split("?").last
+
     new(
-      id:   "omg",
+      id:   competitor_params["compid"],
       name: competitor_div.text
     )
   end
