6b974c605d344bd3b507c1a6acc793225efacd2e
ciscou <francismpp@gmail.com>
2014-02-13 23:00:13 +0100

    exception notifications

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ gem 'jquery-rails'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 
+gem 'exception_notification'
+
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,10 @@ CubecompsMobile::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.middleware.use ExceptionNotification::Rack, email: {
+    email_prefix: "[Cubecomps mobile] ",
+    sender_address: %{"notifier" <notifier@m.cubecomps.com>},
+    exception_recipients: %w[francismpp@gmail.com]
+  }
 end
