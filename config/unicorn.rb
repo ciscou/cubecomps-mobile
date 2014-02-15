4009052dddbb5eab86cd7bd8a39ebe7bc925234b
ciscou <francismpp@gmail.com>
2014-02-15 12:12:19 +0100

    reconnect redis on unicorn fork

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,10 +7,14 @@ before_fork do |server, worker|
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
   end
+
+  $redis.quit
 end
 
 after_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
+
+  $redis = Redis.new url: (ENV["REDISTOGO_URL"] || "redis://localhost:6379")
 end
