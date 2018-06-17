48670a6f21218e96ea0cafaa06845349609d5b3a
ciscou <francismpp@gmail.com>
2018-06-17 13:11:56 +0200

    new twitter event names

--- app/models/records_publisher.rb
+++ app/models/records_publisher.rb
@@ -89,18 +89,18 @@ class RecordsPublisher
 
   def publishable_event_name(event_name)
     {
-      "3x3x3 Cube: Blindfolded"          => "3x3x3 Bld",
-      "3x3x3 Cube: One-handed"           => "3x3x3 OH",
-      "3x3x3 Cube: Fewest moves"         => "3x3x3 FMC",
-      "3x3x3 Cube: With Feet"            => "3x3x3 WF",
-      "2x2x2 Cube"                         => "2x2x2",
-      "4x4x4 Cube"                         => "4x4x4",
-      "5x5x5 Cube"                         => "5x5x5",
-      "6x6x6 Cube"                         => "6x6x6",
-      "7x7x7 Cube"                         => "7x7x7",
-      "4x4x4 Cube: Blindfolded"            => "4x4x4 Bld",
-      "5x5x5 Cube: Blindfolded"            => "5x5x5 Bld",
-      "3x3x3 Cube: Multiple Blindfolded" => "3x3x3 Multi Bld",
+      "3x3x3 Blindfolded"  => "3x3x3 Bld",
+      "3x3x3 One-handed"   => "3x3x3 OH",
+      "3x3x3 Fewest moves" => "3x3x3 FMC",
+      "3x3x3 With Feet"    => "3x3x3 WF",
+      "2x2x2 Cube"         => "2x2x2",
+      "4x4x4 Cube"         => "4x4x4",
+      "5x5x5 Cube"         => "5x5x5",
+      "6x6x6 Cube"         => "6x6x6",
+      "7x7x7 Cube"         => "7x7x7",
+      "4x4x4 Blindfolded"  => "4x4x4 Bld",
+      "5x5x5 Blindfolded"  => "5x5x5 Bld",
+      "3x3x3 Multi-Blind"  => "3x3x3 Multi Bld",
     }.fetch(event_name, event_name)
   end
 
