b930bb396a60bf107a9c009c98412747793f2b24
ciscou <francismpp@gmail.com>
2018-09-19 16:19:11 +0200

    force ssl on production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  force_ssl if Rails.env.production?
+
   rescue_from NotFoundException, ActionView::MissingTemplate do |ex|
     raise ActionController::RoutingError.new("Not Found")
   end
