91bf936371d518700778d7c09cae60f6bb424b2c
ciscou <francismpp@gmail.com>
2018-09-21 11:33:46 +0200

    preparing to prefetch all records by competition

--- app/models/round.rb
+++ app/models/round.rb
@@ -149,20 +149,26 @@ class Round
 
     if results.any?(&:world_record?)
       $redis.sadd "world_records", redis_key
+      $redis.sadd "world_records:#{competition_id}", redis_key
     else
       $redis.srem "world_records", redis_key
+      $redis.srem "world_records:#{competition_id}", redis_key
     end
 
     if results.any?(&:continental_record?)
       $redis.sadd "continental_records", redis_key
+      $redis.sadd "continental_records:#{competition_id}", redis_key
     else
       $redis.srem "continental_records", redis_key
+      $redis.srem "continental_records:#{competition_id}", redis_key
     end
 
     if results.any?(&:national_record?)
       $redis.sadd "national_records", redis_key
+      $redis.sadd "national_records:#{competition_id}", redis_key
     else
       $redis.srem "national_records", redis_key
+      $redis.srem "national_records:#{competition_id}", redis_key
     end
   rescue Redis::CannotConnectError => e
     ExceptionNotifier.notify_exception(e)
