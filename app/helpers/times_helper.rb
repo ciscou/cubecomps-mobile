6924d1242947cc808b86db7580ecb603840b03a2
ciscou <francismpp@gmail.com>
2018-10-05 14:14:26 +0200

    ooops

--- app/helpers/times_helper.rb
+++ app/helpers/times_helper.rb
@@ -1,5 +1,7 @@
 module TimesHelper
   def format_time(time)
+    return time if time.nil?
+
     # removing leading and trailing &nbsp;
     time.gsub(/\A[[:space:]]/, "").gsub(/[[:space:]]\z/, "")
   end
